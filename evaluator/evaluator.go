bdb044f5bbec0fefd38b430ebeb870e746aea6c0
Drew Rodman <dcrodman@gmail.com>
2020-05-08 13:53:50 -0700

    Add infix and prefix expression evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,7 +21,14 @@ func Eval(node ast.Node) object.Object {
 	case *ast.Integer:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return evalBoolean(node)
+		return boolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	}
 	return nil
 }
@@ -36,8 +43,92 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	return result
 }
 
-func evalBoolean(b *ast.Boolean) *object.Boolean {
-	if b.Value {
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	}
+	return NULL
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+	// TODO: Expressions?
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	val := right.(*object.Integer).Value
+	return &object.Integer{Value: -val}
+}
+
+func evalInfixExpression(
+	operator string,
+	left object.Object,
+	right object.Object,
+) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return boolToBooleanObject(left == right)
+	case operator == "!=":
+		return boolToBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(
+	operator string,
+	left object.Object,
+	right object.Object,
+) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return intToIntegerObject(leftVal + rightVal)
+	case "-":
+		return intToIntegerObject(leftVal - rightVal)
+	case "*":
+		return intToIntegerObject(leftVal * rightVal)
+	case "/":
+		return intToIntegerObject(leftVal / rightVal)
+	case "<":
+		return boolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return boolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return boolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return boolToBooleanObject(leftVal != rightVal)
+	default:
+		return NULL
+	}
+}
+
+func intToIntegerObject(val int64) *object.Integer {
+	return &object.Integer{Value: val}
+}
+
+func boolToBooleanObject(val bool) *object.Boolean {
+	if val {
 		return TRUE
 	}
 	return FALSE
